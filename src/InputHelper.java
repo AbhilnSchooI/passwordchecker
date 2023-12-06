395f677ad3a7732e874c2083c87ae51e0f7a4c6d
AbhiGone <giriabhinaya@gmail.com>
2023-12-06 09:43:54 -0500

    goodpassword

--- src/InputHelper.java
+++ src/InputHelper.java
@@ -1,2 +1,188 @@
-package PACKAGE_NAME;public class inputhelper {
+import java.util.Scanner;
+
+public class InputHelper {
+    public static void main(String[] args) {
+        //Test Methods
+        Scanner scan = new Scanner(System.in);
+    }
+
+    //this method loops until a valid int is received, returns int value.
+    public static int getInt(Scanner scan, String prompt) {
+        boolean done = false;
+        int x = 0;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextInt()) {
+                x = scan.nextInt();
+                done = true;
+            } else {
+                System.out.println("Invalid input. Please try again");
+            }
+            scan.nextLine(); //clear buffer
+        } while (!done);
+        return x;
+    }
+
+    //this method loops until a valid ranged int is received, returns int value
+    public static int getRangedInt(Scanner scan, int min, int max, String prompt) {
+        boolean done = false;
+        int x = 0;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextInt()) {
+                x = scan.nextInt();
+                if (x <= max && x >= min) {
+                    done = true;
+                } else {
+                    System.out.println("Invalid range. Please try again");
+                }
+            }
+            scan.nextLine(); //clear buffer
+        } while (!done);
+        return x;
+    }
+
+    //this method loops until a double is entered.
+    public static double getDouble(Scanner scan, String prompt) {
+        boolean done = false;
+        double x = 0;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextDouble()) {
+                x = scan.nextDouble();
+                done = true;
+            } else {
+                System.out.println("You have entered an invalid Double. Please try again");
+            }
+            scan.nextLine();
+        } while (!done);
+
+        return x;
+    }
+
+    public static double getRangedDouble(Scanner scan, double min, double max, String prompt) {
+        boolean done = false;
+        double x = 0;
+
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextDouble()) {
+                x = scan.nextDouble();
+                if (x <= max && x >= min) {
+                    done = true;
+                } else {
+                    System.out.println("Invalid range. Please try again");
+                }
+            }
+            scan.nextLine(); //clear buffer
+        } while (!done);
+        return x;
+    }
+
+
+    //this returns a boolean if you want to continue which is true and false if not.
+    public static boolean YNConfirm(Scanner scan, String prompt) {
+        boolean done = false;
+        boolean con = false;
+        String check;
+        System.out.println(prompt);
+
+        do {
+            check = scan.nextLine();
+            if (check.equalsIgnoreCase("Y")) {
+                con = true;
+                done = true;
+            } else if (check.equalsIgnoreCase("N")) {
+                con = false;
+                done = true;
+            } else {
+                System.out.println("You have entered an invalid input. Please try again");
+            }
+        } while (!done);
+        return con;
+    }
+
+
+    public static String getRegExString(Scanner scan, String prompt, String regEx) {
+        String input;
+        boolean valid = false;
+
+
+        do {
+            System.out.println(prompt);
+            input = scan.nextLine();
+            if (input.matches(regEx)) {
+                valid = true;
+            } else {
+                System.out.println("Invalid input.");
+            }
+        } while (!valid);
+
+        return input;
+    }
+
+    public static int getPositiveNonZeroInt(Scanner scan, String prompt) {
+        boolean done = false;
+        int x = 0;
+        System.out.println(prompt);
+        do {
+            if (scan.hasNextInt()) {
+                x = scan.nextInt();
+                if (x > 0) {
+                    done = true;
+                } else {
+                    System.out.println("Invalid input");
+                }
+            } else {
+                System.out.println("Invalid input");
+            }
+            scan.nextLine();
+        } while (!done);
+        return x;
+    }
+
+    public static String getNonZeroLenString(Scanner scan, String prompt){
+        boolean done = false;
+        String str = "";
+        System.out.println(prompt);
+
+        do {
+            if(scan.hasNextLine()){
+                str = scan.nextLine();
+                if(str.length() > 0){
+                    done = true;
+                }else{
+                    System.out.println("Invalid input");
+                }
+            }
+        } while (!done);
+        return str;
+    }
+
+    public static void prettyHeader(String msg) {
+        String x = " ";
+        int y = msg.length();
+        int z = (60-y)/2 -2 ;
+        for (int i = 0; i < 60; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+        for (int i = 0; i < 60; i++) {
+            if (z == i) {
+                System.out.print(msg);
+            } else if (i < 3 || i > 56) {
+                System.out.print("*");
+            } else if (i < 56 - (msg.length()-2)) {
+                System.out.print(x);
+            }
+        }
+        System.out.println();
+        for (int i = 0; i < 60; i++) {
+            System.out.print("*");
+        }
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,105 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        boolean done = false;
+        boolean specialChar = false;
+        boolean upperCase = false;
+        boolean lowerCase = false;
+        boolean num = false;
+        boolean keepgo = false;
+
+        String password = "";
+        String current = "";
+        int count = 0;
+
+        do {
+
+            System.out.println("Please enter your password:");
+            password = scan.nextLine();
+
+            //checks length
+            if (password.length() < 12) {
+                System.out.println("Your password should be atleast 12 characters long");
+            } else {
+                count++;
+            }
+
+            //checks for uppercase
+            for (int i = 0; i < password.length(); i++) {
+                current = password.substring(i, i + 1);
+                if (current.matches("[A-Z]")) {
+                    upperCase = true;
+                }
+            }
+
+            if (!upperCase) {
+                System.out.println("Your password should have atleast oneupper case character");
+            } else {
+                count++;
+            }
+
+            //checks for lowercase
+            for (int i = 0; i < password.length(); i++) {
+                current = password.substring(i, i + 1);
+                if (current.matches("[a-z]")) {
+                    lowerCase = true;
+                }
+            }
+
+            if (!lowerCase) {
+                System.out.println("Your password should have atleast one lowercase character");
+            } else {
+                count++;
+            }
+
+            //checks for number
+            for (int i = 0; i < password.length(); i++) {
+                current = password.substring(i, i + 1);
+                if (current.matches("[0-9]")) {
+                    num = true;
+                }
+            }
+
+            if (!num) {
+                System.out.println("Your password should have atleast one number");
+            } else {
+                count++;
+            }
+
+            //checks special char
+
+            for (int i = 0; i < password.length(); i++) {
+                current = password.substring(i, i + 1);
+                if (current.matches("[!#$%]")) {
+                    specialChar = true;
+                }
+            }
+
+            if (!specialChar) {
+                System.out.println("Your password should have atleast one special character [!@#$%^&*()]");
+            } else {
+                count++;
+            }
+
+            System.out.println("Your password passed " + count + "/5 tests.");
+
+            if(count <= 2){
+                System.out.println("Your password's strength is weak");
+            } else if(count == 3) {
+                System.out.println("Your password's strength is medium");
+            }else if(count >= 4){
+                System.out.println("Your password's strength is strong");
+            }
+
+            keepgo = InputHelper.YNConfirm(scan,"Would you like to check for another password? [Y/N]");
+            if(keepgo == false){
+                done = true;
+            }
+
+        }while (!done) ;
     }
 }
